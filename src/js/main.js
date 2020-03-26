63f88c23b28e705ed91a6471c2a129347969ca1a
loukass23 <l.dupias@net.estia.fr>
2020-03-26 12:02:52 +0100

    carousel edits

--- src/js/main.js
+++ src/js/main.js
@@ -2,20 +2,14 @@ import prepare from './structure/prepare';
 import '../style.scss';
 
 window.scrollTo(0, 2);
-
-
-
 document.onkeydown = function (e) {
-    console.log('e.keyCode :', e.keyCode);
-
     switch (e.keyCode) {
         case 37:
-            console.log('nesxt :');
             $("#carouselProjects").carousel("next");
             break;
         case 39:
             $("#carouselProjects").carousel("prev");
             break;
     }
-}
+};
 prepare();
